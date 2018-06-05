5f23a1a376c9bfcef5ab8c55a2f2c4aee98389e7
yosuke.adachi <yosuke.adachi@avanade.com>
2018-06-05 22:37:26 +0900

    Add project files.

---
+++ MCP70480jsMVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MCP70480jsMVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ MCP70480jsMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MCP70480jsMVC.MvcApplication" Language="C#" %>
