bb5a3ad0ab3b3de58e5cf4708665966887f700ad
yosuke.adachi <yosuke.adachi@avanade.com>
2018-06-25 21:59:53 +0900

    custome error

--- MCP70480jsMVC/Controllers/HomeController.cs
+++ MCP70480jsMVC/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace MCP70480jsMVC.Controllers
 {
@@ -10,6 +6,7 @@ namespace MCP70480jsMVC.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Message = "Home Action Method.";
             return View();
         }
 
@@ -30,6 +27,13 @@ namespace MCP70480jsMVC.Controllers
         public ActionResult Test()
         {
             return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult PlayVideo()
+        {
+            ViewBag.Message = "Playing Intro Video";
+            return View("Index");
         }
     }
 }
