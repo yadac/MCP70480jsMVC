efccfea78c6b79e969710a037a94570bc2d6ca61
yosuke.adachi <doruji1982@gmail.com>
2018-06-28 21:38:34 +0900

    DisplayModeProvider

--- MCP70480jsMVC/Global.asax.cs
+++ MCP70480jsMVC/Global.asax.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.WebPages;
 
 namespace MCP70480jsMVC
 {
@@ -16,6 +13,12 @@ namespace MCP70480jsMVC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // for each Device type
+            DisplayModeProvider.Instance.Modes.Insert(0, new DefaultDisplayMode("iPhone")
+            {
+                ContextCondition = (c => c.Request.UserAgent.IndexOf("iPhone") >= 0)
+            });
         }
     }
 }
