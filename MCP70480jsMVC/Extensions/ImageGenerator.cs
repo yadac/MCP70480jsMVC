e2d216e7f12871ab1bc860b3e3b2866f7db93606
yosuke.adachi <doruji1982@gmail.com>
2018-06-28 23:16:30 +0900

    IHttpHandler

---
+++ MCP70480jsMVC/Extensions/ImageGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+
+namespace MCP70480jsMVC.Extensions
+{
+    public class ImageGenerator : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write("custom http handler says hello");
+        }
+
+        public bool IsReusable => true;
+    }
+}
