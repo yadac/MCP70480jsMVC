26f1788a94ae1f9049c64c59c7c845449bc8599c
yosuke.adachi <doruji1982@gmail.com>
2018-07-02 21:28:43 +0900

    regexp js

--- MCP70480jsMVC/Scripts/ysk-0.1.0-common.js
+++ MCP70480jsMVC/Scripts/ysk-0.1.0-common.js
@@ -5,6 +5,18 @@
         var first = $('#first').val();
         $('#uinfos').text("hello, " + first + " " + last);
 
+        checkPhoneNumber();
+
         event.preventDefault();
     });
+
+    function checkPhoneNumber() {
+        console.log("check phone number");
+        var rgx = new RegExp(/^1?[\s\(-]\d{3}[\s\)-]?\d{3}[\s)-]?\d{4}$/);
+        console.log(`rgx.test('1-800-555-6677') is ${rgx.test('1-800-555-6677')}`);
+        console.log(`rgx.test('11-800-555-6677') is ${rgx.test('11-800-555-6677')}`);
+        console.log(`rgx.test('1(800)555 6677') is ${rgx.test('1(800)555 6677')}`);
+    }
+
+    
 })
