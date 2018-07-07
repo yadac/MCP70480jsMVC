5075722a367c14f7aad36699a758df2a49d3cb0d
yosuke.adachi <doruji1982@gmail.com>
2018-07-07 19:46:35 +0900

    DisplayNameAttribute

--- For70486/Models/Member.cs
+++ For70486/Models/Member.cs
@@ -1,14 +1,20 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace For70486.Models
 {
     public class Member
     {
         public int Id { get; set; }
+        [DisplayName("氏名")]
         public string Name { get; set; }
+        [DisplayName("メールアドレス")]
         public string Email { get; set; }
+        [DisplayName("誕生日")]
         public DateTime Birth { get; set; }
+        [DisplayName("既婚")]
         public bool Married { get; set; }
+        [DisplayName("備考")]
         public string Memo { get; set; }
     }
 }
