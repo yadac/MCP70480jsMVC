016cd5e6a3090fa1d9606970f20004952d431f1e
yosuke.adachi <doruji1982@gmail.com>
2018-07-08 22:16:59 +0900

    iHttpHandler

--- For70486/App_Start/RouteConfig.cs
+++ For70486/App_Start/RouteConfig.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using For70486.Extensions;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -12,6 +9,10 @@ namespace For70486
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // ex. http://{domain}/action/a?size=100
+            var customRoute = new Route("action/{something}", new MyCustomHandler());
+            routes.Add(customRoute);
 
             routes.MapRoute(
                 name: "Default",
---
+++ For70486/Extensions/MyCustomHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Web;
+using System.Web.Routing;
+
+namespace For70486.Extensions
+{
+    public class MyCustomHandler : IHttpHandler, IRouteHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            var p1 = context.Request["size"];
+            var p2 = context.Request.Params["size"];
+            context.Response.Write($"parameter is {p1}({p2})");
+        }
+
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+
+        public IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            return this;
+        }
+    }
+}
