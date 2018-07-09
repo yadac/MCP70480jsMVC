8f5dcb07dd8957e31be11aa312366f4a2099cf79
yosuke.adachi <doruji1982@gmail.com>
2018-07-09 22:33:08 +0900

    input values

--- For70486/Controllers/HomeController.cs
+++ For70486/Controllers/HomeController.cs
@@ -29,10 +29,14 @@ namespace For70486.Controllers
         }
 
         [HttpPost]
+        // [ValidateInput(false)] // inputに対するvalidationが実行されなくなる
         public ActionResult Question(string question)
         {
+            // querystringやformの要素は下記のように取得できる
+            var req1 = Request["question1"];
             // viewの値をパラメータで受け取るためには, inputのnameの値とパラメータ名を一致させる
-            return Content(question);
+            var ret = question + " - " + req1;
+            return Content(ret);
         }
     }
 }
